b5bdd967bf997dc1f232c4f5941f76b3c9e8398a
ItoMasahiro <mitarashidangoman@gmail.com>
2018-08-25 02:10:39 +0900

    初期コミット

--- src/app/entry-points/ep-main-page/ep-main.component.ts
+++ src/app/entry-points/ep-main-page/ep-main.component.ts
@@ -18,7 +18,7 @@ export class EpMainComponent implements OnInit {
     const elems = document.getElementById('epTypeHeader').children;
     for (let i = 0; i < elems.length; i++) {
       if (i == type) {
-        elems[i].setAttribute('class', 'item active');
+        elems[i].setAttribute('class', 'item active'); 
       } else {
         elems[i].setAttribute('class', 'item');
       }
